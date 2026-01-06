13c359a5130d2ec54ddfd938178e81f769bf881b
Kenzie Gibson <gibsonkenzie76@gmail.com>
2026-01-06 12:51:52 +0000

    First Commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_website.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
